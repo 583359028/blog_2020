66609d10e89b2441b08ba691100508ef237800a2
xiao <dream@xiaodeMacBook-Pro.local>
2020-02-13 18:45:48 +0800

    管理员可以删除用户

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -18,6 +18,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'Dreams';
         $user->email = '583359028@qq.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
