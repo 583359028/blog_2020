6e33bec43dabd939ade2163091b6bc8ee944aaf5
xiao <dream@xiaodeMacBook-Pro.local>
2020-02-18 22:42:57 +0800

    heroku test

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -103,6 +103,7 @@ class UsersController extends Controller
     public function confirmEmail($token)
     {
         $user = User::where('activation_token', $token)->firstOrFail();
+        dd($user);
         $user->activated = true;
         $user->activation_token = null;
         $user->save();
