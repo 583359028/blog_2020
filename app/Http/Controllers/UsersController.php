74393ed15b44a4ea0051d008fb2bfabac5a0d222
xiao <dream@xiaodeMacBook-Pro.local>
2020-02-18 21:42:37 +0800

    移除邮件发送的 from 选项

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -47,6 +47,7 @@ class UsersController extends Controller
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
+            'email_verified_at' => time(),
             'password' => bcrypt($request->password),
         ]);
 
@@ -59,13 +60,11 @@ class UsersController extends Controller
     {
         $view = 'emails.confirm';
         $data = compact('user');
-        $from = '583359028@qq.com';
-        $name = 'Dreams';
         $to = $user->email;
         $subject = "感谢注册 Weibo 应用！请确认你的邮箱。";
 
-        Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject){
-            $message->from($from, $name)->to($to)->subject($subject);
+        Mail::send($view, $data, function ($message) use ( $to, $subject){
+            $message->to($to)->subject($subject);
         });
     }
 
