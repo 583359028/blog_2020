e4e89bdadb83e6e82c4616bc8501dd9a3a7e42f1
xiao <dream@xiaodeMacBook-Pro.local>
2020-02-25 23:43:05 +0800

    社交统计信息

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -5,7 +5,10 @@
 <div class="row">
     <div class="offset-md-2 col-md-8">
                 <section class="user_info">
-                    @include('shared._user_info',['user'=>$user])
+                    @include('shared._user_info',['user' => $user])
+                </section>
+                <section class="stats mt-2">
+                    @include('shared._status', ['user' => $user])
                 </section>
                 <section class="status">
                     @if ($statuses->count() > 0)
